d057632ed31e1179fc1e6d17cecacfa309405f1f
yeinson <yeinsondiaz31@gmail.com>
2022-11-04 20:39:00 -0500

    commit 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import Compclase from "./componentes/Compclase";
 import Compfuncion from "./componentes/Compfuncion";
 import Parrafo from "./componentes/Parrafo";
 import Variables from "./componentes/Variables";
-import Eventos from "./componentes/Eventos";
+
 import Estados from "./componentes/Estados";
 import Contador from "./componentes/Contador";
-import Listas from "./componentes/Listas";
 import { Formulario } from "./componentes/Formulario";
 function App() {
   return (
@@ -16,10 +15,10 @@ function App() {
     <Compfuncion/>
     <Parrafo/>
     <Variables></Variables>
-    <Eventos></Eventos>
+
     <Estados></Estados>
     <Contador></Contador>
-    <Listas/>
+
     
     </div>
   );
--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -8,7 +8,7 @@ const Contador = () => {
     <div>
         <h2>Contador</h2>
         <h3>Contador: {contador}</h3>
-        <h4>{contador%2==0 ? 'Es par': 'Es impar'}</h4>
+        <h4>{contador%2===0 ? 'Es par': 'Es impar'}</h4>
         <button className='btn btn-primary ms-2' onClick={()=>aumentar()}>Aumentar</button>
         <button className='btn btn-success ms-2' onClick={()=>disminuir()}>Disminuir</button>
     </div>
